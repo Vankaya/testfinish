aa3d7578acdbfeec9c2fbdeb155ddfffeddb57f1
vankaya <van4ka7@gmailcom>
2020-02-27 17:26:09 +0200

    fix

--- testchat/settings.py
+++ testchat/settings.py
@@ -137,5 +137,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
